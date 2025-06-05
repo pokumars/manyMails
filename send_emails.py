51afe7b16086470f57b2f9eaad4fe3a4569e6aa8
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2025-06-05 09:37:08 +0300

    de-anonymise some data

--- send_emails.py
+++ send_emails.py
@@ -10,13 +10,14 @@ sender_email = os.getenv("sender_email")
 sender_password = os.getenv("sender_password")
 
 # Define the subject and attachments
-subject = "Experienced Professional for Civil Service - test-user Surname"
-attachments = ["cv_test-user_Surname_sivari.pdf", "cl_test-.pdf"]
+subject = "Subject"
+attachments = ["cv_filename.pdf", "cl_filename.pdf"]
 
 # List of recipients
 recipients = [
-    {"name": "Outlook test-user", "email": "test-userkob@hotmail.com"},
-    {"name": "Gmail test-user", "email": "test-userkob@gmail.com"},
+    {"name": "Firstname Lastname", "email": "random@yahoo.com"},
+    {"name": "Firstname2 Lastname2", "email": "random2@yahoo.com"},
+    {"name": "Firstname3 Lastname3", "email": "random3@yahoo.com"},
     # Add more recipients as needed
 ]
 
@@ -24,8 +25,8 @@ recipients = [
 yag = yagmail.SMTP(
     user=sender_email, 
     password=sender_password,
-    host='smtp.mail.yahoo.com',
-    port=587,
+    host='smtp.mail.yahoo.com', #find your correct host
+    port=587, #find your correct port
     smtp_ssl=False,
     smtp_starttls=True
 )
@@ -33,17 +34,18 @@ yag = yagmail.SMTP(
 # Send emails
 for recipient in recipients:
     body = (
-        f"Hi {recipient['name']},<br><br>"
+        "I speak fluent finnish btw.<br><br>"
+        f"Hi {recipient['name'].split()[0]},<br><br>"        
         "I am a 27-year-old experienced software developer with a host of other useful skills, "
-        "and I would love to do my <b>2-month</b> civil service for Aalto startup centre. This is because "
+        "and I would love to do my <b>2-month</b> civil service for your institution. This is because "
         "I am deeply interested in entrepreneurship, innovation, and Finland's startup ecosystem. "
         "I have attached a cover letter and CV - <b>let me solve some problems for you.</b><br><br>"
-        "Best regards,<br><br>"
-        "test-user Surname<br><br>"
-        "test-userkob@yahoo.com | +358 44 282 6020<br><br>"
+        "Best regards,<br>"
+        "Firstname Lastname<br>"
+        "random@yahoo.com | +358 0123456789<br>"
         "Helsinki<br><br>"
         '<a href="https://dev.to/pokumars">Dev blog/articles</a> | '
-        '<a href="https://www.linkedin.com/in/test-user-pm/">LinkedIn</a> | '
+        '<a href="https://www.linkedin.com/in/oheneba-pm/">LinkedIn</a> | '
         '<a href="https://github.com/pokumars">GitHub</a>'
     )
     yag.send(
